0f57fb537b342d945030be17f6d04b886f66ddbb
Stuart Gill <stuart.a.gill@gmail.com>
2019-02-12 10:20:44 -0800

    first commit, includes code for help queue, all tickets still hard coded

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+//Ticket gets its props passed to it from TicketList. (data down action up). In React these props are a special type of argument, since all components are functions
+function Ticket(props) {
+  return (
+    <div>
+      <style jsx>{`
+        div {
+          background-color: red;
+        }
+      `}</style>
+      <h3>
+        {props.location} - {props.names}
+      </h3>
+      <p>
+        <em>{props.issue}</em>
+      </p>
+      <hr />
+    </div>
+  );
+}
+
+//PropTypes dependency allows us to declare a list of props (and data types thereof) that Ticket component accepts
+//PropTypes (cap P) refers to class we imported
+//propTypes (lower) declares a propTypes property on the Ticket component. This property is an object literal containing key-value pairs.
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string
+};
+
+export default Ticket;
+
+// VARIOUS TYPES OF PROPTYPES:
+// MyExampleComponent.propTypes = {
+//     exampleArray: PropTypes.array,
+//     exampleBoolean: PropTypes.bool,
+//     exampleFunction: PropTypes.func,
+//     exampleNumber: PropTypes.number,
+//     exampleObject: PropTypes.object,
+//     exampleString: PropTypes.string,
+//     exampleSymbol: PropTypes.symbol,
+//     exampleReactElement: PropTypes.element,
+//     exampleArrayOfNumbers: PropTypes.arrayOf(PropTypes.number),
+//     exampleArrayOfStrings: PropTypes.arrayOf(PropTypes.string),
+//     exampleClassTypeProp: PropTypes.instanceOf(ExampleClassName),
+//     optionalEnum: PropTypes.oneOf(['ExampleClass', 'AnotherExampleClass'])
+//   }
---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+//wrapper component that handles reloading and sending errors
+import { AppContainer } from "react-hot-loader";
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById("react-app-root")
+  );
+};
+
+render(App);
+
+//this actually triggers the swapping process
+//the eslint tags disable so "undefined" module doesn't throw an error
+/*eslint-disable */
+if (module.hot) {
+  module.hot.accept("./components/App", () => {
+    render(App);
+  });
+}
+/*eslint-enable */
