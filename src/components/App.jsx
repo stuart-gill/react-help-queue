5e54fb3682cdc38ae59a894aad3213ded5c4e3cf
stuart gill <stuart.a.gill@gmail.com>
2019-02-20 16:20:59 -0800

    add admin component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import TicketList from "./TicketList";
 import NewTicketControl from "./NewTicketControl";
 import Error404 from "./Error404";
 import Moment from "moment";
+import Admin from './Admin';
 
 //Route is primary building block of ReactRouter
 //Switch determines which route matches the path the user is requesting... if we have multiple routes, they should should reside within a switch components, which allows the router to iterate through them and locate correct route
@@ -33,7 +34,7 @@ class App extends React.Component {
       60000
     );
   }
-  
+
   updateTicketElapsedWaitTime() {
 
     let newMasterTicketList = this.state.masterTicketList.slice();
@@ -76,6 +77,7 @@ class App extends React.Component {
               />
             )}
           />
+          <Route path='/admin' render={() => <Admin ticketList={this.state.masterTicketList} />} />
           <Route component={Error404} />
         </Switch>
       </div>
